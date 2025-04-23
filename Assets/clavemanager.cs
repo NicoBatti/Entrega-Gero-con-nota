3a53c3f9bada20b122f6b37879916a13737b1a09
NicoBatti <nicolasbattias@gmail.com>
2025-04-23 15:39:32 -0300

    codigo terminado
    cuando ingresas una clave que no va, te dice que te denega el acceso

---
+++ Assets/clavemanager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class clavemanager : MonoBehaviour
+{
+    public TMP_InputField inputClave;
+    public TextMeshProUGUI txtMensaje;
+
+    string clave = "tic2025";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtMensaje.text = "";   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ComprobarClaveIngresada()
+    {
+        string claveIngresada = inputClave.text;
+        if (claveIngresada != clave)
+        {
+            txtMensaje.text = "Acceso denegado";
+        }
+        else
+        {
+            txtMensaje.text = "Le damos la bienvenida";
+        }
+    }
+}
